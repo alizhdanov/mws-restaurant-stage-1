0995abea81d9ac5db14cb9711727fa68bf460b99
Ali Zhdanov <ali.zhdanov@kiwi.com>
2018-05-19 16:05:44 +0200

    Added offline support for api calls

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -2,10 +2,35 @@
  * Common database helper functions.
  */
 
-const Store = new Map()
+const storeName = 'restaraunts-store';
+const storeVersion = 1;
+const objectStoreName = 'api';
+
+const dbPromise = idb.open(storeName, storeVersion, upgradeDB => {
+  upgradeDB.createObjectStore(objectStoreName);
+});
+
+const idbRestaraunts = {
+  get(key) {
+    return dbPromise.then(db => {
+      return db
+        .transaction(objectStoreName)
+        .objectStore(objectStoreName)
+        .get(key);
+    });
+  },
+  set(key, val) {
+    return dbPromise.then(db => {
+      const tx = db.transaction(objectStoreName, 'readwrite');
+      tx.objectStore(objectStoreName).put(val, key);
+      return tx.complete;
+    });
+  },
+};
+
+const Store = new Map();
 
 class DBHelper {
-
   /**
    * Database URL.
    * Change this to restaurants.json file location on your server.
@@ -14,7 +39,31 @@ class DBHelper {
     // const port = 8000 // Change this to your server port
     // return `${window.location.origin}/data/restaurants.json`;
 
-    return `http://localhost:9999/restaurants`
+    return `http://localhost:1337/restaurants`;
+  }
+
+  static getData(query) {
+    if (Store.has(query)) {
+      return Promise.resolve(Store.get(query));
+    } else {
+      const promise = fetch(query)
+        .then(res => {
+          if (!res.ok) {
+            throw Error(response.statusText);
+          }
+          return res.json();
+        })
+        .catch(err => idbRestaraunts.get(query));
+
+      Store.set(query, promise);
+
+      return promise.then(res => {
+        Store.set(query, res);
+        console.log(res);
+        idbRestaraunts.set(query, res); // todo try to fix it
+        return res;
+      });
+    }
   }
 
   /**
@@ -22,38 +71,26 @@ class DBHelper {
    */
   static fetchRestaurants(params) {
     let query = this.DATABASE_URL;
-    if(params && Object.keys(params).length) {
-      console.log(params)
+    if (params && Object.keys(params).length) {
       query += '?';
       const paramsArray = [];
-      for(let key in params) {
-        paramsArray.push(`${key}=${params[key]}`)
+      for (let key in params) {
+        paramsArray.push(`${key}=${params[key]}`);
       }
 
-      query += paramsArray.join('&')
+      query += paramsArray.join('&');
     }
 
-    if(Store.has(query)) {
-      return Promise.resolve(Store.get(query))
-    } else {
-      return fetch(query)
-        .then(res => res.json())
-        .then(res => {
-          Store.set(query, res)
-          return res
-        })
-        .catch(err => { throw Error(err) })
-    }
+    return this.getData(query);
   }
 
   /**
    * Fetch a restaurant by its ID.
    */
   static fetchRestaurantById(id, callback) {
-    return fetch(`${this.DATABASE_URL}/${id}`)
-      .then(res => res.json())
+    return this.getData(`${this.DATABASE_URL}/${id}`)
       .then(restaurant => callback(null, restaurant))
-      .catch(err => callback('Restaurant does not exist', null))
+      .catch(err => callback('Restaurant does not exist', null));
   }
 
   /**
@@ -63,62 +100,42 @@ class DBHelper {
     // Fetch all restaurants  with proper error handling
     this.fetchRestaurants({ cousine_type: cuisine })
       .then(restaurants => callback(null, restaurants))
-      .catch(err => callback(err, null))
+      .catch(err => callback(err, null));
   }
 
   /**
    * Fetch restaurants by a neighborhood with proper error handling.
    */
   static fetchRestaurantByNeighborhood(neighborhood, callback) {
-    // Fetch all restaurants
-    // DBHelper.fetchRestaurants((error, restaurants) => {
-    //   if (error) {
-    //     callback(error, null);
-    //   } else {
-    //     // Filter restaurants to have only given neighborhood
-    //     const results = restaurants.filter(r => r.neighborhood == neighborhood);
-    //     callback(null, results);
-    //   }
-    // });
-
     this.fetchRestaurants({ neighborhood })
       .then(restaurants => callback(null, restaurants))
-      .catch(err => callback(err, null))
+      .catch(err => callback(err, null));
   }
 
   /**
    * Fetch restaurants by a cuisine and a neighborhood with proper error handling.
    */
-  static fetchRestaurantByCuisineAndNeighborhood(cuisine, neighborhood, callback) {
+  static fetchRestaurantByCuisineAndNeighborhood(
+    cuisine,
+    neighborhood,
+    callback
+  ) {
     // Fetch all restaurants
-    // DBHelper.fetchRestaurants((error, restaurants) => {
-    //   if (error) {
-    //     callback(error, null);
-    //   } else {
-    //     let results = restaurants
-    //     if (cuisine != 'all') { // filter by cuisine
-    //       results = results.filter(r => r.cuisine_type == cuisine);
-    //     }
-    //     if (neighborhood != 'all') { // filter by neighborhood
-    //       results = results.filter(r => r.neighborhood == neighborhood);
-    //     }
-    //     callback(null, results);
-    //   }
-    // });
-
     const params = {};
 
-    if (cuisine !== 'all') { // filter by cuisine
+    if (cuisine !== 'all') {
+      // filter by cuisine
       params.cuisine_type = cuisine;
     }
 
-    if (neighborhood !== 'all') { // filter by neighborhood
-      params.neighborhood = neighborhood
+    if (neighborhood !== 'all') {
+      // filter by neighborhood
+      params.neighborhood = neighborhood;
     }
 
     this.fetchRestaurants(params)
       .then(restaurants => callback(null, restaurants))
-      .catch(err => callback(err, null))
+      .catch(err => callback(err, null));
   }
 
   /**
@@ -128,11 +145,13 @@ class DBHelper {
     // // Fetch all restaurants
     this.fetchRestaurants().then(restaurants => {
       // Get all neighborhoods from all restaurants
-      const neighborhoods = restaurants.map(v => v.neighborhood)
+      const neighborhoods = restaurants.map(v => v.neighborhood);
       // Remove duplicates from neighborhoods
-      const uniqueNeighborhoods = neighborhoods.filter((v, i) => neighborhoods.indexOf(v) === i)
+      const uniqueNeighborhoods = neighborhoods.filter(
+        (v, i) => neighborhoods.indexOf(v) === i
+      );
       callback(null, uniqueNeighborhoods);
-    })
+    });
   }
 
   /**
@@ -142,32 +161,36 @@ class DBHelper {
     // Fetch all restaurants
     this.fetchRestaurants().then(restaurants => {
       // Get all cuisines from all restaurants
-      const cuisines = restaurants.map(v => v.cuisine_type)
+      const cuisines = restaurants.map(v => v.cuisine_type);
       // Remove duplicates from cuisines
-      const uniqueCuisines = cuisines.filter((v, i) => cuisines.indexOf(v) === i)
+      const uniqueCuisines = cuisines.filter(
+        (v, i) => cuisines.indexOf(v) === i
+      );
       callback(null, uniqueCuisines);
-    })
+    });
   }
 
   /**
    * Restaurant page URL.
    */
   static urlForRestaurant(restaurant) {
-    return (`./restaurant.html?id=${restaurant.id}`);
+    return `/restaurant.html?id=${restaurant.id}`;
   }
 
   /**
    * Restaurant image URL.
    */
   static imageUrlForRestaurant(restaurant) {
-    return restaurant.photograph ? (`/img/${restaurant.photograph}.jpg`) : '/img/no-image.jpg';
+    return restaurant.photograph
+      ? `/img/${restaurant.photograph}.jpg`
+      : '/img/no-image.jpg';
   }
 
   /**
    * Restaurant image alt text.
    */
   static imageAltTextForRestaurant(restaurant) {
-    return (`${restaurant.name}'s thumbnails`);
+    return `${restaurant.name}'s thumbnails`;
   }
   /**
    * Map marker for a restaurant.
@@ -178,9 +201,8 @@ class DBHelper {
       title: restaurant.name,
       url: DBHelper.urlForRestaurant(restaurant),
       map: map,
-      animation: google.maps.Animation.DROP}
-    );
+      animation: google.maps.Animation.DROP,
+    });
     return marker;
   }
-
 }
--- js/toast.js
+++ js/toast.js
@@ -1,123 +1,129 @@
-const generateTemplate = ({text, buttons}) => {
-    const div = document.createElement('div');
-    div.classList.add('toast');
-    div.setAttribute('tabindex', '-1');
-    div.innerHTML = `
+const generateTemplate = ({ text, buttons }) => {
+  const div = document.createElement('div');
+  div.classList.add('toast');
+  div.setAttribute('tabindex', '-1');
+  div.innerHTML = `
       <div id="toast-message" class="toast-content">${text}</div>
-      ${buttons.map((name) => `<button class="unbutton">${name}</button>`).join('')}
-    `
-    return div
+      ${buttons
+        .map(name => `<button class="unbutton">${name}</button>`)
+        .join('')}
+    `;
+  return div;
 };
 
 class Toast {
-    constructor(text, duration, buttons) {
-        this.trapTabKey = this.trapTabKey.bind(this)
-        this.hide = this.hide.bind(this)
+  constructor(text, duration, buttons) {
+    this.trapTabKey = this.trapTabKey.bind(this);
+    this.hide = this.hide.bind(this);
 
-        this.container = generateTemplate({
-            text,
-            buttons
-        })
+    this.container = generateTemplate({
+      text,
+      buttons,
+    });
 
-        this.answer = new Promise((resolve) => {
-            this._answerResolver = resolve;
-        });
+    this.answer = new Promise(resolve => {
+      this._answerResolver = resolve;
+    });
 
-        this.gone = new Promise((resolve) => {
-            this._goneResolver = resolve;
-        });
+    this.gone = new Promise(resolve => {
+      this._goneResolver = resolve;
+    });
 
-        if (duration) {
-            this._hideTimeout = setTimeout(() => {
-                this.hide();
-            }, duration);
-        }
-
-        this.container.addEventListener('click', (event) => {
-            const button = event.target.closest('button');
-            if (!button) return;
-            this._answerResolver(button.textContent);
-            this.hide();
-        });
-
-        this.focusedElementBeforeModal = document.activeElement;
-        this.container.addEventListener('keydown', this.trapTabKey);
-        const focusableElementsString = 'a[href], area[href], input:not([disabled]), select:not([disabled]), textarea:not([disabled]), button:not([disabled]), iframe, object, embed, [tabindex="0"], [contenteditable]';
-        let focusableElements = this.container.querySelectorAll(focusableElementsString);
-        // Convert NodeList to Array
-        focusableElements = Array.prototype.slice.call(focusableElements);
-
-        this.firstTabStop = focusableElements[0];
-        this.lastTabStop = focusableElements[focusableElements.length - 1];
+    if (duration) {
+      this._hideTimeout = setTimeout(() => {
+        this.hide();
+      }, duration);
     }
 
-    hide () {
-        clearTimeout(this._hideTimeout);
-        this._answerResolver();
-        this._goneResolver();
+    this.container.addEventListener('click', event => {
+      const button = event.target.closest('button');
+      if (!button) return;
+      this._answerResolver(button.textContent);
+      this.hide();
+    });
 
-        this.container.classList.remove('show');
+    this.focusedElementBeforeModal = document.activeElement;
+    this.container.addEventListener('keydown', this.trapTabKey);
+    const focusableElementsString =
+      'a[href], area[href], input:not([disabled]), select:not([disabled]), textarea:not([disabled]), button:not([disabled]), iframe, object, embed, [tabindex="0"], [contenteditable]';
+    let focusableElements = this.container.querySelectorAll(
+      focusableElementsString
+    );
+    // Convert NodeList to Array
+    focusableElements = Array.prototype.slice.call(focusableElements);
 
-        this.focusedElementBeforeModal.focus();
+    this.firstTabStop = focusableElements[0];
+    this.lastTabStop = focusableElements[focusableElements.length - 1];
+  }
 
-        return this.gone;
-    };
+  hide() {
+    clearTimeout(this._hideTimeout);
+    this._answerResolver();
+    this._goneResolver();
 
-    trapTabKey (e) {
-        // Check for TAB key press
-        if (e.keyCode === 9) {
+    this.container.classList.remove('show');
 
-            // SHIFT + TAB
-            if (e.shiftKey) {
-                if (document.activeElement === this.firstTabStop) {
-                    e.preventDefault();
-                    this.lastTabStop.focus();
-                }
+    this.focusedElementBeforeModal.focus();
 
-                // TAB
-            } else {
-                if (document.activeElement === this.lastTabStop) {
-                    e.preventDefault();
-                    this.firstTabStop.focus();
-                }
-            }
+    return this.gone;
+  }
+
+  trapTabKey(e) {
+    // Check for TAB key press
+    if (e.keyCode === 9) {
+      // SHIFT + TAB
+      if (e.shiftKey) {
+        if (document.activeElement === this.firstTabStop) {
+          e.preventDefault();
+          this.lastTabStop.focus();
         }
 
-        // ESCAPE
-        if (e.keyCode === 27) {
-            this.hide();
+        // TAB
+      } else {
+        if (document.activeElement === this.lastTabStop) {
+          e.preventDefault();
+          this.firstTabStop.focus();
         }
+      }
     }
+
+    // ESCAPE
+    if (e.keyCode === 27) {
+      this.hide();
+    }
+  }
 }
 
 class Toasts {
-    constructor(appendToEl) {
-        this._container = document.createElement('div');
-        this._container.classList.add('toasts');
-        this._container.setAttribute('role', 'alertdialog')
-        this._container.setAttribute('aria-labelledby', 'toast-message')
-        appendToEl.appendChild(this._container);
-    }
+  constructor(appendToEl) {
+    this._container = document.createElement('div');
+    this._container.classList.add('toasts');
+    this._container.setAttribute('role', 'alertdialog');
+    this._container.setAttribute('aria-labelledby', 'toast-message');
+    appendToEl.appendChild(this._container);
+  }
 
-    show (message, opts) {
-        opts = Object.assign({}, {
-            duration: 0,
-            buttons: ['dismiss']
-        }, opts);
+  show(message, opts) {
+    opts = Object.assign(
+      {},
+      {
+        duration: 0,
+        buttons: ['dismiss'],
+      },
+      opts
+    );
 
-        const toast = new Toast(message, opts.duration, opts.buttons);
-        this._container.appendChild(toast.container);
+    const toast = new Toast(message, opts.duration, opts.buttons);
+    this._container.appendChild(toast.container);
 
-        toast.container.classList.add('show');
+    toast.container.classList.add('show');
 
-        debugger
+    toast.firstTabStop.focus();
 
-        toast.firstTabStop.focus()
+    toast.gone.then(() => {
+      toast.container.parentNode.removeChild(toast.container);
+    });
 
-        toast.gone.then(() => {
-            toast.container.parentNode.removeChild(toast.container);
-        });
-
-        return toast;
-    };
+    return toast;
+  }
 }
