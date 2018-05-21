4b103f88130b6318a204d0df2a3b636b1c6cfe59
Ali Zhdanov <ali.zhdanov@kiwi.com>
2018-05-21 22:25:41 +0200

    Add more nicer build step

--- build.js
+++ build.js
@@ -1,6 +1,11 @@
 const fs = require('fs');
 const path = require('path');
 const sqip = require('sqip');
+const critical = require('critical');
+
+///////////////////////////////////
+// create lazy thumbnails
+//////////////////////////////////
 
 const makePlaceholder = filename => {
   const file = sqip({
@@ -20,4 +25,26 @@ fs.readdir('./img/', (err, files) => {
   files
     .filter(file => file.endsWith('jpg'))
     .forEach(file => makePlaceholder(file));
+});
+
+///////////////////////////////////
+// generate critical css
+//////////////////////////////////
+
+critical.generate({
+  inline: true,
+  base: '.',
+  src: './index_src.html',
+  dest: './index.html',
+  width: 1300,
+  height: 900,
+});
+
+critical.generate({
+  inline: true,
+  base: '.',
+  src: './restaurant_src.html',
+  dest: './restaurant.html',
+  width: 1300,
+  height: 900,
 });
